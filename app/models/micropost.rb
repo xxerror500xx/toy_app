f14af26df520450e37e9b742cab3b72ae1f09424
Cliff Minks <Cliff.Minks@gmail.com>
2016-02-21 17:03:26 -0600

    Fix validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,6 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :content, length: { maximum: 140 },
                       presence: true
-  validates :user, presence: true
+  validates :user_id, presence: true
 
 end
